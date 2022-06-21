eb7f22663b2253cfc0e0fb99c47f5da8752c6eb2
André Montanari <montanari.andre@aluno.ifsp.edu.br>
2022-06-20 23:21:41 -0300

    projeto ToDoAngular

---
+++ src/app/ToDo.ts
@@ -0,0 +1,4 @@
+export class ToDo{
+    name:string;
+    isCompleted:boolean;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToDo } from './ToDo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  todos : ToDo[] = [];
+  newTodo : string;
+
+  saveToDo(){
+    if(this.newTodo){
+      let todo = new ToDo()
+      todo.name = this.newTodo
+      todo.isCompleted = true
+      this.todos.push(todo)
+      this.newTodo = ''
+
+    }else{
+      alert('Favor digitar o ToDo')
+    }
+  }
+
+  
+  done(id:number){
+    this.todos[id].isCompleted = !this.todos[id].isCompleted
+  }
+
+  remove(id:number){
+    this.todos = this.todos.filter((v,i) => i !== id)
+  }
+
+
 }
